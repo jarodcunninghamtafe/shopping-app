393b1a4e0393ee569a58cb290cc7c7a4a6000037
jarodcunninghamtafe <jarod.cunningham4@studytafensw.edu.au>
2024-11-04 13:09:15 +1100

    add fetch

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { Surface, Text, Switch } from "react-native-paper";
+import { fetchAllCategories } from "../utils/api";
 
 export default function HomeScreen(props) {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchCategories = await fetchAllCategories();
+      setCategories(fetchCategories);
+    };
+    fetchData();
+  }, []);
+
   return (
     <Surface style={styles.container}>
       <Text variant="displaySmall" style={styles.title}>
         Home Screen
       </Text>
+      {categories.map((category) => (
+        <Text key={category.id}>{category.name}</Text>
+      ))}
     </Surface>
   );
 }
